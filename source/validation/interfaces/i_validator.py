93f3b96b20d348849c81855003928cbfe71c2a3f
Schwarf <andreas.b.scharf@gmail.com>
2021-05-22 13:41:27 +0200

    Remove obsolete validation folder.

--- source/validation/interfaces/i_validator.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from pure_interface import Interface, abstractmethod
-
-
-class IValidator(Interface):
-    @abstractmethod
-    def validate(self, input: Any) -> None:
-        pass
