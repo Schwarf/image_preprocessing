c70f65b15275a7ef0e94c9a0918350588795830d
Schwarf <andreas.b.scharf@gmail.com>
2021-05-22 11:40:23 +0200

    Add tensorflow record writer.

---
+++ source/data_access/files/implementations/tensorflow_record_writer.py
@@ -0,0 +1,22 @@
+import os
+
+import tensorflow
+from data_access.files.interfaces.i_file_writer import IFileWriter
+
+
+class TensorflowRecordWriter(IFileWriter):
+    def __init__(self, path_to_file: str):
+        self._file_handle = None
+        if not os.path.exists(os.path.dirname(path_to_file)):
+            raise OSError("The path p rovided to the constructor of TensorflowRecordWriter does not exist!")
+        self._path_to_file = path_to_file
+
+    def open(self) -> None:
+        self._file_handle = tensorflow.io.TFRecordWriter(self._path_to_file)
+
+    def write(self, data: tensorflow.train.Example()):
+        self._file_handle.write(data.SerializeToString())
+
+    def close(self):
+        self._file_handle.flush()
+        self._file_handle.close()
