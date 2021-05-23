773d63a92ed58e8bfb7a5eb2c30218c763d1e209
Schwarf <andreas.b.scharf@gmail.com>
2021-05-23 19:44:41 +0200

    Refactor file writer.

--- source/data_access/files/implementations/tensorflow_record_writer.py
+++ source/data_access/files/implementations/tensorflow_record_writer.py
@@ -5,13 +5,12 @@ from data_access.files.interfaces.i_file_writer import IFileWriter
 
 
 class TensorflowRecordWriter(IFileWriter, object):
-    def __init__(self, path_to_file: str):
+    def __init__(self):
         self._file_handle = None
-        if not os.path.exists(os.path.dirname(path_to_file)):
-            raise OSError("The path provided to the constructor of TensorflowRecordWriter does not exist!")
-        self._path_to_file = path_to_file
 
-    def open(self) -> None:
+    def open(self, path_to_file) -> None:
+        if not os.path.exists(os.path.dirname(path_to_file)):
+            raise ValueError("The path provided to the 'open' method of TensorflowRecordWriter does not exist!")
         self._file_handle = tensorflow.io.TFRecordWriter(self._path_to_file)
 
     def write(self, data: tensorflow.train.Example()):
