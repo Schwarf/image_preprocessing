43c1cbb44932fa169b235e6081df91e6e1ca1d35
Schwarf <andreas.b.scharf@gmail.com>
2021-07-15 12:56:09 +0200

    Make it work on ubuntu.

--- source/scripts/app_create_json_with_image_files.py
+++ source/scripts/app_create_json_with_image_files.py
@@ -1,7 +1,7 @@
 import json
 import os
 from typing import List
-
+from source.data_access.files.implementations.json_file_reader import JSONFileReader
 
 def get_list_of_files(dir_name: str) -> List[str]:
     list_of_directories = os.listdir(dir_name)
@@ -19,8 +19,9 @@ def get_list_of_files(dir_name: str) -> List[str]:
 
 
 def create_json_label_file(file_list: List[str], image_path: str) -> None:
-    path = "D:/Data/image_data/labels/"
-    json_file_name = os.path.basename(image_path) + ".json"
+    #path = "D:/Data/image_data/labels/"
+    path = "/media/linux_data/data/image_data/labels/"
+    json_file_name = os.path.basename(image_path) + "_linux.json"
     default_label_value = -1
     default_label_dict = {os.path.normpath(file_name): default_label_value for file_name in file_list}
     file_handle = open(path + json_file_name, "w+")
@@ -28,7 +29,28 @@ def create_json_label_file(file_list: List[str], image_path: str) -> None:
     file_handle.close()
 
 
-image_path = "D:/Data/image_data/images/my_camera"
-file_list = get_list_of_files(image_path)
+def replace_win_path_with_linux_path():
+    label_file_path = ""
+    windows_path = ""
+    linux_path = ""
+    reader = JSONFileReader()
+    reader.open(label_file_path)
+    data = reader.get_data()
+    new_data = {}
+    for key, value in data.items():
+        new_key = key.replace(windows_path, linux_path)
+        new_key = new_key.replace("\\", "/")
+        new_data[new_key] = data[key]
+    path = ""
+
+    json_file_name = os.path.basename(path) + "my_camera_labeling_linux.json"
+    file_handle = open(path + json_file_name, "w+")
+    json.dump(new_data, file_handle, indent=4)
+
+
+replace_win_path_with_linux_path()
+#image_path = "D:/Data/image_data/images/my_camera"
+#image_path = "/media/linux_data/data/image_data/labels/"
+#file_list = get_list_of_files(image_path)
 
 # create_json_label_file(file_list, image_path)
